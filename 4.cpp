a22581649619b182679eb9caa6d4f49204a0d08d
maryam^c <maryam.abbasi.6549@gmail.com>
2016-12-08 09:40:52 +0330

    ASCLL

---
+++ 4.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+char character;
+cout<<"enter a character: ";
+cin>>character;
+cout<<"the ASCII code for this character is : "<<(int) character<<"\n";
+  return 0;
+}
